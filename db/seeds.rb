920d0a3a41e56876f16a9217d353b4f5f963b13f
Cornelius Heggli <cornelius.heggli@gmail.com>
2022-11-10 15:33:47 +0800

    create controllers lists, bookmarks and create nested routes

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+puts 'Destroying all movies...'
+Movie.destroy_all
+
+puts 'Seeding new movies...'
+50.times do
+  movie = Movie.create(
+    title: Faker::Movie.title,
+    overview: Faker::Quote.famous_last_words,
+    poster_url: Faker::LoremFlickr.image(size: "200x240", search_terms: ['movies'])    ,
+    rating: rand(0.0..10.0).round(1)
+  )
+  puts "seeding - #{movie.title} - with rating: #{movie.rating}"
+end
+
+puts 'Finished seeding!!!'
