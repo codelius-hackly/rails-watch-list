540edf2fb07065fe329f57b6aa5946f6dee4625f
Cornelius Heggli <cornelius.heggli@gmail.com>
2022-11-11 15:43:52 +0800

    added cloudinary

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,6 +1,8 @@
 class List < ApplicationRecord
+  validates :name, presence: true, uniqueness: true
+
   has_many :bookmarks, dependent: :destroy
   has_many :movies, through: :bookmarks
 
-  validates :name, presence: true, uniqueness: true
+  has_one_attached :image
 end
